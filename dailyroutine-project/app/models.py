225a75df54ef16da7ac18cbac23deffdd5b67445
MonoSetton <maksymiliankloch@gmail.com>
2023-05-19 21:36:11 +0200

    Added CRUD functionality

--- dailyroutine-project/app/models.py
+++ dailyroutine-project/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    completed = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
--- dailyroutine-project/app/views.py
+++ dailyroutine-project/app/views.py
@@ -1,6 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from django.contrib.auth.decorators import login_required
+from .forms import TaskForm
+from django.core.exceptions import BadRequest
 
 
+@login_required(login_url='/login')
 def home(request):
-    context = {}
+    tasks = Task.objects.filter(author=request.user)
+    context = {"tasks": tasks}
     return render(request, 'app/home.html', context)
+
+
+@login_required(login_url='/login')
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user
+            form.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+    return render(request, 'app/create_task.html', {'form': form})
+
+
+@login_required(login_url='/login')
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    if task.author == request.user:
+        if request.method == 'POST':
+            form = TaskForm(request.POST, instance=task)
+            if form.is_valid():
+                form.save()
+                return redirect('/')
+        return render(request, 'app/update_task.html', {'form': form})
+    else:
+        raise BadRequest("You do not have permission to see this site")
+
+
+def delete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.author == request.user:
+        if request.method == 'POST':
+            task.delete()
+            return redirect('/')
+        return render(request, 'app/delete_task.html', {'task': task})
+    else:
+        raise BadRequest("You do not have permission to see this site")
+
+
