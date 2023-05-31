305e4527be686aced925ba699e483d9c69f80a5d
MonoSetton <70628883+MonoSetton@users.noreply.github.com>
2023-05-31 15:38:43 +0200

    Update urls.py

--- dailyroutine-project/dailyroutine/urls.py
+++ dailyroutine-project/dailyroutine/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('', include('app.urls')),
     path('', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
